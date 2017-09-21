05fba73255d22939494cfc4692d5aad266b18f75
Yang Wu <ywu613@gmail.com>
2017-09-21 16:31:41 -0400

    simple ipv4 tcp client-server system without timeout

--- programming-assignment1/client.c
+++ programming-assignment1/client.c
@@ -1,8 +1,8 @@
 /*///////////////////////////////////////////////////////////
 *
 * FILE:		client.c
-* AUTHOR:	Your Name Here
-* PROJECT:	CS 3251 Project 1 - Professor Ellen Zegura 
+* AUTHOR:	Yang Wu
+* PROJECT:	CS 3251 Project 1 - Professor Ellen Zegura
 * DESCRIPTION:	Network Client Code
 * CREDIT:	Adapted from Professor Traynor
 *
@@ -10,69 +10,94 @@
 
 /* Included libraries */
 
-#include <stdio.h>		    /* for printf() and fprintf() */
-#include <sys/socket.h>		    /* for socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>		    /* for sockaddr_in and inet_addr() */
+#include <stdio.h>          /* for printf() and fprintf() */
+#include <sys/socket.h>     /* for socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>      /* for sockaddr_in and inet_addr() */
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
 /* Constants */
-#define RCVBUFSIZE 512		    /* The receive buffer size */
-#define SNDBUFSIZE 512		    /* The send buffer size */
+#define RCVBUFSIZE 512      /* The receive buffer size */
+#define SNDBUFSIZE 512      /* The send buffer size */
 #define REPLYLEN 32
 
 /* The main function */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    int clientSock;		    /* socket descriptor */
-    struct sockaddr_in serv_addr;   /* server address structure */
+    int clientSock;                 /* socket descriptor */
+    struct sockaddr_in servAddr;    /* server address structure */
 
-    char *accountName;		    /* Account Name  */
-    char *servIP;		    /* Server IP address  */
-    unsigned short servPort;	    /* Server Port number */
-    
+    char *accountName;              /* Account Name  */
+    char *servIP;                   /* Server IP address  */
+    unsigned short servPort;        /* Server Port number */
 
-    char sndBuf[SNDBUFSIZE];	    /* Send Buffer */
-    char rcvBuf[RCVBUFSIZE];	    /* Receive Buffer */
-    
-    int balance;		    /* Account balance */
+    char sndBuf[SNDBUFSIZE];        /* Send Buffer */
+    char rcvBuf[RCVBUFSIZE];        /* Receive Buffer */
 
-    /* Get the Account Name from the command line */
-    if (argc != 4) 
-    {
-	printf("Incorrect number of arguments. The correct format is:\n\taccountName serverIP serverPort");
-	exit(1);
-    }
+    int balance;                    /* Account balance */
+
     accountName = argv[1];
     memset(&sndBuf, 0, SNDBUFSIZE);
     memset(&rcvBuf, 0, RCVBUFSIZE);
 
     /* Get the addditional parameters from the command line */
-    /*	    FILL IN	*/
+    servIP = argv[2];               /* Get the server IP address */
+    servPort = atoi(argv[3]);       /* Get the server port number */
+    char *command = argv[4];
+    char *amount = (argc == 6) ? argv[5] : "0";
 
-    /* Create a new TCP socket*/
-    /*	    FILL IN	*/
-
+    /* Create a new TCP socket */
+    clientSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSock < 0) {
+        printf("Fail to create socket\n");
+        exit(1);
+    }
 
     /* Construct the server address structure */
-    /*	    FILL IN	 */
-
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr(servIP);
+    servAddr.sin_port = htons(servPort);
 
     /* Establish connecction to the server */
-    /*	    FILL IN	 */
+    if (connect(clientSock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+        printf("Connect failed\n");
+        exit(1);
+    }
 
-    
-    /* Send the string to the server */
-    /*	    FILL IN	 */
+    int sendBytes = 0;
+    int recvBytes = 0;
 
+    /* Send account name to the server, and wait for the response */
+    sendBytes = send(clientSock, accountName, strlen(accountName), 0);
+    recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+
+    /* Send the command to the server, and wait for the response */
+    memset(&rcvBuf, 0, RCVBUFSIZE);
+    sendBytes = send(clientSock, command, strlen(command), 0);
+    recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+
+    /* Send the amount to the server, and receive the new balance */
+    if (argc == 6) {
+        memset(&rcvBuf, 0, RCVBUFSIZE);
+        sendBytes = send(clientSock, amount, strlen(amount), 0);
+        recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+        printf("%s\n", rcvBuf); /* Print the withdraw message */
+
+        /* Ask for the current balance */
+        strcpy(sndBuf, "BAL");
+        memset(&rcvBuf, 0, RCVBUFSIZE);
+        sendBytes = send(clientSock, sndBuf, strlen(sndBuf), 0);
+        recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+    }
+
+    balance = atoi(rcvBuf);
+    close(clientSock);
 
     /* Receive and print response from the server */
-    /*	    FILL IN	 */
-
-    printf("%s\n", accountName);
-    printf("Balance is: %i\n", balance);
+    printf("Account Name: %s, ", accountName);
+    printf("and the current Balance: %i\n", balance);
 
     return 0;
 }
--- programming-assignment1/server.c
+++ programming-assignment1/server.c
@@ -1,8 +1,8 @@
 /*///////////////////////////////////////////////////////////
 *
 * FILE:		server.c
-* AUTHOR:	Your Name Here
-* PROJECT:	CS 3251 Project 1 - Professor Ellen Zegura 
+* AUTHOR:	Yang Wu
+* PROJECT:	CS 3251 Project 1 - Professor Ellen Zegura
 * DESCRIPTION:	Network Server Code
 * CREDIT:	Adapted from Professor Traynor
 *
@@ -10,59 +10,173 @@
 
 /*Included libraries*/
 
-#include <stdio.h>	  /* for printf() and fprintf() */
-#include <sys/socket.h>	  /* for socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>	  /* for sockaddr_in and inet_addr() */
-#include <stdlib.h>	  /* supports all sorts of functionality */
-#include <unistd.h>	  /* for close() */
-#include <string.h>	  /* support any string ops */
+#include <stdio.h>          /* for printf() and fprintf() */
+#include <sys/socket.h>     /* for socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>      /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>         /* supports all sorts of functionality */
+#include <unistd.h>         /* for close() */
+#include <string.h>         /* support any string ops */
 
-#define RCVBUFSIZE 512		/* The receive buffer size */
-#define SNDBUFSIZE 512		/* The send buffer size */
-#define BUFSIZE 40		/* Your name can be as many as 40 chars*/
+#define RCVBUFSIZE 512      /* The receive buffer size */
+#define SNDBUFSIZE 512      /* The send buffer size */
+#define BUFSIZE 40          /* Your name can be as many as 40 chars*/
+#define BALANCESIZE 10      /* The balance buffer size */
 
 /* The main function */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    int serverSock;				/* Server Socket */
-    int clientSock;				/* Client Socket */
-    struct sockaddr_in changeServAddr;		/* Local address */
-    struct sockaddr_in changeClntAddr;		/* Client address */
-    unsigned short changeServPort;		/* Server port */
-    unsigned int clntLen;			/* Length of address data struct */
+    int serverSock;                         /* Server Socket */
+    int clientSock;                         /* Client Socket */
+    struct sockaddr_in changeServAddr;      /* Local address */
+    struct sockaddr_in changeClntAddr;      /* Client address */
+    unsigned short changeServPort;          /* Server port */
+    unsigned int clntLen;                   /* Length of address data struct */
 
-    char nameBuf[BUFSIZE];			/* Buff to store account name from client */
-    int  balance;				/* Place to record account balance result */
+    char nameBuf[BUFSIZE];                  /* Buff to store account name from client */
+    int  balance;                           /* Place to record account balance result */
 
+    /* Pre-configure each account balance */
+    int myCheckingBalance   = 3000;
+    int mySavingsBalance    = 5000;
+    int myRetirementBalance = 10000;
+    int myCollegeBalance    = 1500;
 
-    /* Create new TCP Socket for incoming requests*/
-    /*	    FILL IN	*/
+    char rcvBuf[RCVBUFSIZE];        /* Buffer to receive message from client */
+    char sndBuf[SNDBUFSIZE];        /* Buffer to send message to client */
+    char balanceBuf[BALANCESIZE];   /* Buffer to store the current balance */
 
-    /* Construct local address structure*/
-    /*	    FILL IN	*/
-    
-    /* Bind to local address structure */
-    /*	    FILL IN	*/
-
-    /* Listen for incoming connections */
-    /*	    FILL IN	*/
-
-    /* Loop server forever*/
-    while(1)
-    {
-
-	/* Accept incoming connection */
-	/*	FILL IN	    */
-
-	/* Extract the account name from the packet, store in nameBuf */
-	/* Look up account balance, store in balance */
-	/*	FILL IN	    */
-
-	/* Return account balance to client */
-	/*	FILL IN	    */
-
+    /* Read the parameters from command line */
+    if (argc > 3) {
+        printf("Too many parameters\n");
+        exit(1);
+    }
+    char *changeServIP = "127.0.0.1";   /* Server IP address */
+    changeServPort = 2017;
+    if (argc == 3) {
+        changeServIP = argv[1];
+        changeServPort = atoi(argv[2]);
+    } else if (argc == 2) {
+        changeServPort = atoi(argv[1]);
     }
 
+    /* Create new TCP Socket for incoming requests */
+    serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSock < 0) {
+        printf("Fail to create socket\n");
+        exit(1);
+    }
+
+    /* Construct local address structure */
+    memset(&changeServAddr, 0, sizeof(changeServAddr));
+    changeServAddr.sin_family = AF_INET;
+    changeServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    changeServAddr.sin_port = htons(changeServPort);
+
+    /* Bind to local address structure */
+    if (bind(serverSock, (struct sockaddr*) &changeServAddr, sizeof(changeServAddr)) < 0) {
+        printf("Bind failed\n");
+        exit(1);
+    }
+
+    /* Listen for incoming connections */
+    if (listen(serverSock, 32) < 0) {
+        printf("Listen failed\n");
+        exit(1);
+    }
+
+    /* Loop server forever*/
+    while (1) {
+        /* Wait for a client to connect */
+        printf("-----------------------------------\n");
+        printf("Waiting for incoming connections...\n");
+        int changeClntAddrLen = sizeof(changeClntAddr);
+        clientSock = accept(serverSock, (struct sockaddr *) &changeClntAddr, &changeClntAddrLen);
+        if (clientSock < 0) {
+            printf("Connection failed.\n");
+        } else {
+            printf("Connection accepted.\n");
+        }
+
+        int sendBytes = 0;
+        int recvBytes = 0;
+
+        /* Extract the account name from the packet, store in nameBuf */
+        memset(&nameBuf, 0, BUFSIZE);
+        recvBytes = recv(clientSock, nameBuf, sizeof(nameBuf), 0);
+
+        /* Look up account balance, store in balance */
+        if (strcmp(nameBuf, "myChecking") == 0) {
+            balance = myCheckingBalance;
+        } else if (strcmp(nameBuf, "mySavings") == 0) {
+            balance = mySavingsBalance;
+        } else if (strcmp(nameBuf, "myRetirement") == 0) {
+            balance = myRetirementBalance;
+        } else if (strcmp(nameBuf, "myCollege") == 0) {
+            balance = myCollegeBalance;
+        }
+
+        /* Print the current account information */
+        printf("Account Name: %s, and the current Balance: %d\n", nameBuf, balance);
+
+        /* Ask the client for the specific operation */
+        memset(&sndBuf, 0, SNDBUFSIZE);
+        strcpy(sndBuf, "Balance or Withdraw");
+        sendBytes = send(clientSock, sndBuf, strlen(sndBuf), 0);
+
+        /* Receive the specific operation */
+        memset(&rcvBuf, 0, RCVBUFSIZE);
+        recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+        if (strcmp(rcvBuf, "WITHDRAW") == 0) {
+
+            printf("Withdrawing money...\n");
+
+            /* Ask for the amount */
+            memset(&sndBuf, 0, SNDBUFSIZE);
+            strcpy(sndBuf, "Please enter the amount");
+            sendBytes = send(clientSock, sndBuf, strlen(sndBuf), 0);
+
+            /* Receive the amount from client */
+            memset(&rcvBuf, 0, RCVBUFSIZE);
+            recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+
+            /* Send the withdraw message to client */
+            memset(&sndBuf, 0, SNDBUFSIZE);
+            int amount = atoi(rcvBuf);
+            if (amount <= balance) {
+                balance -= amount;
+                strcpy(sndBuf, "Withdraw Succeeded!");
+            } else {
+                strcpy(sndBuf, "Withdraw Failed!");
+            }
+            sendBytes = send(clientSock, sndBuf, strlen(sndBuf), 0);
+
+            /* Print the current account information */
+            printf("Account Name: %s, and the current Balance: %d\n", nameBuf, balance);
+
+            /* Update the balance of the specific account */
+            if (strcmp(nameBuf, "myChecking") == 0) {
+                myCheckingBalance = balance;
+            } else if (strcmp(nameBuf, "mySavings") == 0) {
+                mySavingsBalance = balance;
+            } else if (strcmp(nameBuf, "myRetirement") == 0) {
+                myRetirementBalance = balance;
+            } else if (strcmp(nameBuf, "myCollege") == 0) {
+                myCollegeBalance = balance;
+            }
+
+            /* Receive the next command from client */
+            memset(&rcvBuf, 0, RCVBUFSIZE);
+            recvBytes = recv(clientSock, rcvBuf, RCVBUFSIZE, 0);
+        }
+
+        if (strcmp(rcvBuf, "BAL") == 0) {
+            memset(&balanceBuf, 0, BALANCESIZE);
+            sprintf(balanceBuf, "%d", balance);
+            sendBytes = send(clientSock, balanceBuf, strlen(balanceBuf), 0);
+        }
+
+        close(clientSock);
+    }
+    return 0;
 }
 
